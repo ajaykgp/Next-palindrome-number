a436aeab84788b76fe8001522441b526276adce0
ajaykgp <ajaykumar.grd@gmail.com>
2012-09-20 01:26:51 +0530

    First Commit

---
+++ nextPalindrome.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#define MAX 50
+
+void nextPalindrome(char s[], int length);
+
+int main()
+{
+	char c, s[MAX];
+	int i = 0, length;
+	while((c = getchar()) != '\n') s[i++] = c; 
+	length = i;
+	nextPalindrome(s, length);
+	for(i = 0; i < length; i++) printf("%c", s[i]);
+	printf("\n");
+	return 0;
+}
+
+void nextPalindrome(char s[], int length)
+{
+	int flag = 0, i = 0, j = length - 1;
+	while(((j - i) != 1) && ((j - i) != 0)){
+		if(s[j] < s[i]) flag = 1;
+		else if(s[j] > s[i]) flag = 0;
+		s[j--] = s[i++]; 
+	}
+	if((j - i) == 1){
+		if(s[i] < s[j]) s[i] = s[j];
+		else if(s[i] > s[j]) s[j] = s[i];
+		else if((s[i] == s[j]) && (flag == 0)){
+			if(s[i] != '9'){
+				s[i]++;
+				s[j]++;
+			}
+			else{
+				do{
+					s[i] = '0';
+					s[j] = '0';
+					i--;
+					j++;
+				}while((i >= 0) && (j < length) && (s[i] == '9') && (s[j] == '9'));
+				s[i]++;
+				s[j]++;
+			}
+		}
+	}
+	if((j - i) == 0){
+		if((flag == 0) && (s[j] != '9')) s[j]++;
+		else if((flag == 0) && (s[j] == '9')){
+			do{
+				s[i] = '0';
+				s[j] = '0';
+				i--;
+				j++;
+			}while((i >= 0) && (j < length) && (s[i] == '9') && (s[j] == '9'));
+			s[i]++;
+			s[j]++;
+		}
+	}
+} 
+	
+
